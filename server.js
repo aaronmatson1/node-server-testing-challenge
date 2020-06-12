fa996fc93b1d6efea90723476f91c30143a411a7
aaronmatson1 <matsonaaron1@gmail.com>
2020-06-11 23:36:24 -0600

    Create, seed, and migrate cars.db

--- server.js
+++ server.js
@@ -5,4 +5,10 @@ const server = express();
 
 server.use(express.json());
 
+server.get("/", (req, res) => {
+    res.json({
+      message: "Welcome to our API!",
+    });
+  });
+
 module.exports = server;
